2b65b821b1321aa5d55ee7a42ae09adde1069e3a
sandra381 <68388896+sandra381@users.noreply.github.com>
2026-02-18 13:59:41 -0600

    Create ComplexityTestClass.java
    Test para probar si la metrica de complexity funciona

---
+++ src/main/java/org/springframework/samples/petclinic/ComplexityTestClass.java
@@ -0,0 +1,31 @@
+package org.springframework.samples.petclinic;
+
+public class ComplexityTestClass {
+
+    // TEMP: prueba de complejidad
+    public int prueba(int x) {
+        int r = 0;
+        if (x == 0) r++;
+        if (x == 1) r++;
+        if (x == 2) r++;
+        if (x == 3) r++;
+        if (x == 4) r++;
+        if (x == 5) r++;
+        if (x == 6) r++;
+        if (x == 7) r++;
+        if (x == 8) r++;
+        if (x == 9) r++;
+        if (x == 10) r++;
+        if (x == 11) r++;
+        if (x == 12) r++;
+        if (x == 13) r++;
+        if (x == 14) r++;
+        if (x == 15) r++;
+        if (x == 16) r++;
+        if (x == 17) r++;
+        if (x == 18) r++;
+        if (x == 19) r++;
+        if (x == 20) r++;
+        return r;
+    }
+}
