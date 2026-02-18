f27d12b9959b692f96f00bc08b193565a62f8f2c
sandra381 <68388896+sandra381@users.noreply.github.com>
2026-02-18 14:13:07 -0600

    Modificacion de test
    Para que pases todos los tests

--- src/main/java/org/springframework/samples/petclinic/ComplexityTestClass.java
+++ src/main/java/org/springframework/samples/petclinic/ComplexityTestClass.java
@@ -11,43 +11,6 @@ public class ComplexityTestClass {
 			r++;
 		if (x == 2)
 			r++;
-		if (x == 3)
-			r++;
-		if (x == 4)
-			r++;
-		if (x == 5)
-			r++;
-		if (x == 6)
-			r++;
-		if (x == 7)
-			r++;
-		if (x == 8)
-			r++;
-		if (x == 9)
-			r++;
-		if (x == 10)
-			r++;
-		if (x == 11)
-			r++;
-		if (x == 12)
-			r++;
-		if (x == 13)
-			r++;
-		if (x == 14)
-			r++;
-		if (x == 15)
-			r++;
-		if (x == 16)
-			r++;
-		if (x == 17)
-			r++;
-		if (x == 18)
-			r++;
-		if (x == 19)
-			r++;
-		if (x == 20)
-			r++;
 		return r;
 	}
-
 }
