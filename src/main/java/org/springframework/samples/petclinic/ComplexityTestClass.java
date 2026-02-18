a662b670d8b678ef5b4680d02f9e4ebc4c026df3
sandra381 <68388896+sandra381@users.noreply.github.com>
2026-02-18 14:16:24 -0600

    Modificacion de test
    Se volvio a modificar el test para que si pasara los tests

--- src/main/java/org/springframework/samples/petclinic/ComplexityTestClass.java
+++ src/main/java/org/springframework/samples/petclinic/ComplexityTestClass.java
@@ -1,16 +1,7 @@
 package org.springframework.samples.petclinic;
 
 public class ComplexityTestClass {
-
-	// TEMP: prueba de complejidad
-	public int prueba(int x) {
-		int r = 0;
-		if (x == 0)
-			r++;
-		if (x == 1)
-			r++;
-		if (x == 2)
-			r++;
-		return r;
-	}
+    public int prueba(int x) {
+        return (x >= 0 && x <= 20) ? 1 : 0;
+    }
 }
